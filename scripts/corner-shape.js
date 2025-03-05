44bcae5c39643631647b8df0624ea651fe49c536
Noam Rosenthal <noamr@users.noreply.github.com>
2025-03-05 09:50:05 +0000

    More cleanups

--- scripts/corner-shape.js
+++ scripts/corner-shape.js
@@ -76,25 +76,25 @@ export function render(style, ctx, width, height) {
   const params = {
     'top-right': {
       outer: [width - style['border-top-right-radius'][0], 0, width, style['border-top-right-radius'][1]],
-      sw: [style['border-top-width'], style['border-right-width']],
+      inset: [style['border-top-width'], style['border-right-width']],
       shape: style['corner-top-right-shape'],
     },
 
     'bottom-right': {
       outer: [width, height - style['border-bottom-right-radius'][1], width - style['border-bottom-right-radius'][0], height],
-      sw: [style['border-right-width'], style['border-bottom-width']],
+      inset: [style['border-right-width'], style['border-bottom-width']],
       shape: style['corner-bottom-right-shape'],
     },
 
     'bottom-left': {
       outer: [style['border-bottom-left-radius'][0], height, 0, height - style['border-bottom-left-radius'][1]],
-      sw: [style['border-bottom-width'], style['border-left-width']],
+      inset: [style['border-bottom-width'], style['border-left-width']],
       shape: style['corner-bottom-left-shape'],
     },
 
     'top-left': {
       outer: [0, style['border-top-left-radius'][1], style['border-top-left-radius'][0], 0],
-      sw: [style['border-left-width'], style['border-top-width']],
+      inset: [style['border-left-width'], style['border-top-width']],
       shape: style['corner-top-left-shape'],
     }
   };
@@ -107,59 +107,32 @@ export function render(style, ctx, width, height) {
     add_corner(ctx, ...outer, shape);
   }
 
-  function tl_first(inner) {
-    return Math.sign(inner[0]) === Math.sign(inner[1])
-  }
-
-  function calc_inner(outer, [sw1, sw2]) {
+  function get_corner(corner) {
+    const {outer, inset, shape} = params[corner];
     const s1 = Math.sign(outer[2] - outer[0]);
     const s2 = Math.sign(outer[3] - outer[1]);
-    return [
+    const [sw1, sw2] = inset;
+    const inner = [
       s1 * sw1, s2 * sw1, -s1 * sw2, -s2 * sw2
     ];
-  }
 
-  function inner_rect(corner) {
-    const {
-      outer,
-      shape,
-      sw
-    } = params[corner];
-    const inner = calc_inner(outer, sw);
+    const tl_first = Math.sign(inner[0]) === Math.sign(inner[1]);
+
     const offset = offset_for_curvature(shape);
-    if (tl_first(inner)) {
+    if (tl_first) {
       const s = offset[1];
       offset[1] = offset[0];
       offset[0] = s;
     }
 
-    return [
+    const inner_rect = [
       outer[0] + inner[0] * offset[0],
       outer[1] + inner[1] * offset[1],
       outer[2] + inner[2] * offset[1],
       outer[3] + inner[3] * offset[0],
     ]
-  }
 
-  function superellipse_center([x0, y0, x1, y1], k) {
-    if (Math.sign(x1 - x0) !== Math.sign(y1 - y0))
-      k = 1 / k;
-    if (k < 1) {
-      [x0, y0, x1, y1] = [x1, y1, x0, y0];
-      k = 1/k;
-    }
-    const {x, y} = se(k);
-    return [x0 + x * (x1 - x0), y0 + (1 - y) * (y1 - y0)]
-  }
-
-  function draw_inner_corner(corner) {
-    const {
-      shape,
-      outer,
-      sw
-    } = params[corner];
-    const inner = calc_inner(outer, sw);
-    const trim = tl_first(inner) ? [
+    const trim = tl_first ? [
       [
         [0, outer[1] + inner[1]],
         [width, outer[1] + inner[1]]
@@ -179,7 +152,38 @@ export function render(style, ctx, width, height) {
       ]
     ];
 
-    add_corner(ctx, ...inner_rect(corner), shape, ...trim);
+    function superellipse_center([x0, y0, x1, y1], k) {
+      if (Math.sign(x1 - x0) !== Math.sign(y1 - y0))
+        k = 1 / k;
+      if (k < 1) {
+        [x0, y0, x1, y1] = [x1, y1, x0, y0];
+        k = 1/k;
+      }
+      const {x, y} = se(k);
+      return [x0 + x * (x1 - x0), y0 + (1 - y) * (y1 - y0)]
+    }
+
+    const outer_center = superellipse_center(outer, shape);
+    const inner_center = superellipse_center(inner_rect, shape);
+    return {
+      outer_rect: outer,
+      inner_rect,
+      inset,
+      shape,
+      inner_offset: inner,
+      outer_center,
+      inner_center,
+      trim
+    }
+  }
+
+  function draw_inner_corner(corner) {
+    const {
+      shape,
+      inner_rect,
+      trim
+    } = get_corner(corner);
+    add_corner(ctx, ...inner_rect, shape, ...trim);
   }
 
   ctx.moveTo(...params['top-right'].outer.slice(0, 2));
@@ -200,52 +204,52 @@ export function render(style, ctx, width, height) {
 
   {
     let {
-      outer,
-      sw,
-      shape
-    } = params["top-left"];
-    const inner = inner_rect('top-left');
+      outer_center,
+      inner_center,
+      inset,
+      inner_rect
+    } = get_corner("top-left");
     ctx.moveTo(0, 0);
-    ctx.lineTo(...superellipse_center(outer, shape))
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(inner[2], inner[1]);
-    ctx.lineTo(inner[2], sw[1]);
+    ctx.lineTo(...outer_center)
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(inner_rect[2], inner_rect[1]);
+    ctx.lineTo(inner_rect[2], inset[1]);
   }
 
   {
     let {
-      outer,
-      sw,
-      shape
-    } = params["top-right"];
-    const inner = inner_rect('top-right');
-    ctx.lineTo(inner[0], sw[0]);
-    ctx.lineTo(inner[0], inner[3]);
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(...superellipse_center(outer, shape))
+      inner_rect,
+      outer_center,
+      inner_center,
+      inset,
+    } = get_corner("top-right");
+    ctx.lineTo(inner_rect[0], inset[0]);
+    ctx.lineTo(inner_rect[0], inner_rect[3]);
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(...outer_center)
     ctx.lineTo(width, 0);
     ctx.fillStyle = style['border-top-color'];
     ctx.fill("nonzero");
 
     ctx.beginPath();
     ctx.lineTo(width, 0);
-    ctx.lineTo(...superellipse_center(outer, shape))
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(inner[0], inner[3]);
-    ctx.lineTo(width - sw[1], inner[3]);
+    ctx.lineTo(...outer_center)
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(inner_rect[0], inner_rect[3]);
+    ctx.lineTo(width - inset[1], inner_rect[3]);
   }
 
   {
     let {
-      outer,
-      sw,
-      shape
-    } = params["bottom-right"];
-    const inner = inner_rect('bottom-right');
-    ctx.lineTo(width - sw[0], inner[1]);
-    ctx.lineTo(inner[2], inner[1]);
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(...superellipse_center(outer, shape))
+      inner_rect,
+      outer_center,
+      inner_center,
+      inset,
+    } = get_corner("bottom-right");
+    ctx.lineTo(width - inset[0], inner_rect[1]);
+    ctx.lineTo(inner_rect[2], inner_rect[1]);
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(...outer_center)
     ctx.lineTo(width, height);
 
     ctx.closePath();
@@ -254,23 +258,23 @@ export function render(style, ctx, width, height) {
 
     ctx.beginPath();
     ctx.lineTo(width, height);
-    ctx.lineTo(...superellipse_center(outer, shape))
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(inner[2], inner[1]);
+    ctx.lineTo(...outer_center)
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(inner_rect[2], inner_rect[1]);
   }
 
 
   {
     let {
-      outer,
-      sw,
-      shape
-    } = params["bottom-left"];
-    const inner = inner_rect('bottom-left');
-    ctx.lineTo(inner[0], height - sw[0]);
-    ctx.lineTo(inner[0], inner[3]);
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(...superellipse_center(outer, shape))
+      outer_center,
+      inner_center,
+      inner_rect,
+      inset,
+    } = get_corner("bottom-left");
+    ctx.lineTo(inner_rect[0], height - inset[0]);
+    ctx.lineTo(inner_rect[0], inner_rect[3]);
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(...outer_center)
     ctx.lineTo(0, height);
 
     ctx.closePath();
@@ -280,23 +284,22 @@ export function render(style, ctx, width, height) {
     ctx.beginPath();
 
     ctx.lineTo(0, height);
-    ctx.lineTo(...superellipse_center(outer, shape))
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(inner[0], inner[3]);
+    ctx.lineTo(...outer_center)
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(inner_rect[0], inner_rect[3]);
   }
 
   {
     let {
-      outer,
-      sw,
-      shape
-    } = params["top-left"];
-    const inner = inner_rect('top-left');
-    ctx.lineTo(sw[0], inner[1]);
-    ctx.lineTo(inner[2], inner[1]);
-    ctx.lineTo(...superellipse_center(inner, shape))
-    ctx.lineTo(...superellipse_center(outer, shape))
-    ctx.lineTo(outer[2], outer[1]);
+      outer_center,
+      inner_center,
+      inner_rect,
+      inset,
+    } = get_corner("top-left");
+    ctx.lineTo(inset[0], inner_rect[1]);
+    ctx.lineTo(inner_rect[2], inner_rect[1]);
+    ctx.lineTo(...inner_center)
+    ctx.lineTo(...outer_center)
     ctx.lineTo(0, 0);
     ctx.closePath();
     ctx.fillStyle = style['border-left-color'];
