17330de0b3ff913e8e576f49eb6fdabcdacc46cc
Noam Rosenthal <noamr@users.noreply.github.com>
2025-03-10 14:38:23 +0000

    Cleanup

--- scripts/corner-params.js
+++ scripts/corner-params.js
@@ -35,50 +35,28 @@ export function resolve_corner_params(style, width, height, outset = null) {
         const s1 = Math.sign(outer[2] - outer[0]);
         const s2 = Math.sign(outer[3] - outer[1]);
         const [sw1, sw2] = inset;
-        const inner = [
+        const inner_offset = [
             s1 * sw1, s2 * sw1, -s1 * sw2, -s2 * sw2
         ];
 
-        const tl_first = Math.sign(inner[0]) === Math.sign(inner[1]);
-
         const offset = offset_for_curvature(shape);
-        if (tl_first) {
-            const s = offset[1];
-            offset[1] = offset[0];
-            offset[0] = s;
+        if (Math.sign(inner_offset[0]) === Math.sign(inner_offset[1])) {
+            offset.reverse();
         }
 
         const inner_rect = [
-            outer[0] + inner[0] * offset[0],
-            outer[1] + inner[1] * offset[1],
-            outer[2] + inner[2] * offset[1],
-            outer[3] + inner[3] * offset[0],
+            outer[0] + inner_offset[0] * offset[0],
+            outer[1] + inner_offset[1] * offset[1],
+            outer[2] + inner_offset[2] * offset[1],
+            outer[3] + inner_offset[3] * offset[0],
         ]
 
-        function superellipse_center([x0, y0, x1, y1], k) {
-            if (Math.sign(x1 - x0) !== Math.sign(y1 - y0))
-                k = 1 / k;
-            if (k < 1) {
-                [x0, y0, x1, y1] = [x1, y1, x0, y0];
-                k = 1 / k;
-            }
-            const {
-                x,
-                y
-            } = se(k);
-            return [x0 + x * (x1 - x0), y0 + (1 - y) * (y1 - y0)]
-        }
-
-        const outer_center = superellipse_center(outer, shape);
-        const inner_center = superellipse_center(inner_rect, shape);
         return [corner, {
             outer_rect: outer,
             shape,
-            outer_center,
             inset,
             inner_rect,
-            inner_offset: inner,
-            inner_center
+            inner_offset,
         }];
     }));
 }
--- scripts/corner-shape.js
+++ scripts/corner-shape.js
@@ -5,7 +5,7 @@ import {
   resolve_corner_params
 } from "./corner-params.js";
 
-function add_corner(ctx, ax, ay, bx, by, curvature) {
+function add_corner(ctx, ax, ay, bx, by, curvature, phase, direction) {
   const vertex = ((bx - ax) * (by - ay) >= 0) ? {
     x: ax,
     y: by
@@ -37,37 +37,47 @@ function add_corner(ctx, ax, ay, bx, by, curvature) {
   }
   ctx.save();
   ctx.transform(hor.x, hor.y, ver.x, ver.y, vertex.x, vertex.y);
-  const [P0, P1, P2, P3, P4, P5, P6] = control_points(curvature);
-  ctx.lineTo(P0.x, P0.y);
-  ctx.bezierCurveTo(P1.x, P1.y, P2.x, P2.y, P3.x, P3.y);
-  ctx.bezierCurveTo(P4.x, P4.y, P5.x, P5.y, P6.x, P6.y);
+  let cp = control_points(curvature);
+  if (phase === "both") {
+    const [P0, P1, P2, P3, P4, P5, P6] = cp;
+    ctx.lineTo(P0.x, P0.y);
+    ctx.bezierCurveTo(P1.x, P1.y, P2.x, P2.y, P3.x, P3.y);
+    ctx.bezierCurveTo(P4.x, P4.y, P5.x, P5.y, P6.x, P6.y);
+  } else {
+    if (phase === "second")
+      cp = cp.slice(3);
+    else
+      cp = cp.slice(0, 4);
+
+    const [P0, P1, P2, P3] = direction === "reverse" ? cp.toReversed() : cp;
+
+    ctx.lineTo(P0.x, P0.y);
+    ctx.bezierCurveTo(P1.x, P1.y, P2.x, P2.y, P3.x, P3.y);
+  }
   ctx.restore();
 }
 
 export function render(style, ctx, width, height) {
   const corner_params = resolve_corner_params(style, width, height);
 
-  function draw_outer_corner(corner) {
-    const {
-      outer_rect,
-      shape
-    } = corner_params[corner];
-    add_corner(ctx, ...outer_rect, shape);
+  function draw_outer_corner(corner, phase = "both", direction) {
+    const params = corner_params[corner];
+    add_corner(ctx, ...params.outer_rect, params.shape, phase, direction);
+  }
+
+  function draw_inner_corner_from_params(params, phase = "both", direction) {
+    add_corner(ctx, ...params.inner_rect, params.shape, phase, direction);
+  }
+
+  function draw_inner_corner(corner, phase = "both", direction) {
+    draw_inner_corner_from_params(corner_params[corner], phase, direction)
   }
 
 
   ctx.scale(0.75, 0.75);
   ctx.translate(60, 60);
 
-  function draw_inner_corner_from_params(params) {
-    add_corner(ctx, ...params.inner_rect, params.shape);
-  }
-
-  function draw_inner_corner(corner) {
-    draw_inner_corner_from_params(corner_params[corner])
-  }
-
-  if (style.shadow) {
+  if (!style.shadow) {
     const {
       spread,
       offset,
@@ -92,127 +102,72 @@ export function render(style, ctx, width, height) {
     draw_inner_corner_from_params(for_shadow['top-left']);
     ctx.lineTo(for_shadow['top-left'].inner_rect[2], -spread);
     ctx.fillStyle = color;
-    ctx.fill("evenodd");
+    ctx.fill("nonzero");
     ctx.closePath();
     ctx.restore();
   }
 
   ctx.beginPath();
 
-  ctx.moveTo(width - style['border-top-right-radius'][0], 0);
   draw_outer_corner('top-right');
   draw_outer_corner('bottom-right');
   draw_outer_corner('bottom-left');
   draw_outer_corner('top-left');
+  ctx.closePath();
   ctx.clip("nonzero");
 
-  ctx.beginPath();
-
   {
-    let {
-      outer_center,
-      inner_center,
-      inset,
-      inner_rect
-    } = corner_params["top-left"];
-    ctx.moveTo(0, 0);
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(inner_rect[2], inner_rect[1]);
-    ctx.lineTo(inner_rect[2], inset[1]);
-  }
-
-  {
-    let {
-      inner_rect,
-      outer_center,
-      inner_center,
-      inset,
-    } = corner_params["top-right"];
-    ctx.lineTo(inner_rect[0], inset[0]);
-    ctx.lineTo(inner_rect[0], inner_rect[3]);
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(width, 0);
+    ctx.beginPath();
+    ctx.moveTo(width - style['border-top-right-radius'][0], 0);
+    draw_outer_corner('top-right', 'first');
+    draw_inner_corner('top-right', 'first', 'reverse');
+    ctx.lineTo(width - style['border-top-right-radius'][0], style['border-top-width']);
+    ctx.lineTo(style['border-top-left-radius'][0], style['border-top-width']);
+    draw_inner_corner('top-left', 'second', 'reverse');
+    draw_outer_corner('top-left', 'second');
     ctx.fillStyle = style['border-top-color'];
-    ctx.fill("nonzero");
-
-    ctx.beginPath();
-    ctx.lineTo(width, 0);
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(inner_rect[0], inner_rect[3]);
-    ctx.lineTo(width - inset[1], inner_rect[3]);
+    ctx.fill();
   }
 
   {
-    let {
-      inner_rect,
-      outer_center,
-      inner_center,
-      inset,
-    } = corner_params["bottom-right"];
-    ctx.lineTo(width - inset[0], inner_rect[1]);
-    ctx.lineTo(inner_rect[2], inner_rect[1]);
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(width, height);
-
-    ctx.closePath();
+    ctx.beginPath();
+    draw_inner_corner('top-right', 'second', 'reverse');
+    draw_outer_corner('top-right', 'second');
+    ctx.lineTo(width, style['border-top-right-radius'][1]);
+    ctx.lineTo(width, height - style['border-bottom-right-radius'][1]);
+    draw_outer_corner('bottom-right', 'first');
+    draw_inner_corner('bottom-right', 'first', 'reverse');
+    ctx.lineTo(width - style['border-right-width'], height - style['border-bottom-right-radius'][1])
+    ctx.lineTo(width - style['border-right-width'], style['border-top-right-radius'][1])
     ctx.fillStyle = style['border-right-color'];
-    ctx.fill("nonzero");
-
+    //    ctx.stroke();
+    ctx.fill();
+  } {
     ctx.beginPath();
-    ctx.lineTo(width, height);
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(inner_rect[2], inner_rect[1]);
-  }
-
-
-  {
-    let {
-      outer_center,
-      inner_center,
-      inner_rect,
-      inset,
-    } = corner_params["bottom-left"];
-    ctx.lineTo(inner_rect[0], height - inset[0]);
-    ctx.lineTo(inner_rect[0], inner_rect[3]);
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(0, height);
-
-    ctx.closePath();
+    ctx.moveTo(width - style['border-bottom-right-radius'][0], height - style['border-bottom-width']);
+    draw_inner_corner('bottom-right', 'second', 'reverse');
+    draw_outer_corner('bottom-right', 'second');
+    ctx.lineTo(width - style['border-bottom-right-radius'][0], height);
+    ctx.lineTo(style['border-bottom-left-radius'][0], height);
+    draw_outer_corner('bottom-left', 'first');
+    draw_inner_corner('bottom-left', 'first', 'reverse');
+    ctx.lineTo(corner_params['bottom-left'].inner_rect[2], height - style['border-bottom-width'])
     ctx.fillStyle = style['border-bottom-color'];
-    ctx.fill("nonzero");
-
-    ctx.beginPath();
-
-    ctx.lineTo(0, height);
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(inner_rect[0], inner_rect[3]);
+    ctx.fill();
   }
 
   {
-    let {
-      outer_center,
-      inner_center,
-      inner_rect,
-      inset,
-    } = corner_params["top-left"];
-    ctx.lineTo(inset[0], inner_rect[1]);
-    ctx.lineTo(inner_rect[2], inner_rect[1]);
-    ctx.lineTo(...inner_center)
-    ctx.lineTo(...outer_center)
-    ctx.lineTo(0, 0);
-    ctx.closePath();
+    ctx.beginPath();
+    ctx.moveTo(0, height - style['border-bottom-left-radius'][1]);
+    draw_outer_corner('bottom-left', 'second', 'reverse');
+    draw_inner_corner('bottom-left', 'second');
+    ctx.lineTo(style['border-left-width'], height - style['border-bottom-left-radius'][1]);
+    ctx.lineTo(style['border-left-width'], style['border-top-left-radius'][1]);
+    draw_inner_corner('top-left', 'first');
+    draw_outer_corner('top-left', 'first', 'reverse');
     ctx.fillStyle = style['border-left-color'];
-    ctx.fill("nonzero");
+    ctx.fill();
   }
-
-  ctx.restore();
 
   const inner_rect = [style['border-left-width'], style['border-top-width'], width - style['border-right-width'], height - style['border-bottom-width']];
   ctx.save();
@@ -247,5 +202,4 @@ export function render(style, ctx, width, height) {
   ctx.fillStyle = "white";
   ctx.fill();
   ctx.restore();
-
 }
